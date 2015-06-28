9c8910d8a67628bcecc5b627e803fa6c77e8af1d
brunohms <bruno_h_m_s@hotmail.com>
2015-06-28 17:37:23 +0100

    bonus_coin added main activity updated to use bonus_coin as a image

--- app/src/main/java/uk/ac/gla/dcs/gms/lms/MainActivity.java
+++ app/src/main/java/uk/ac/gla/dcs/gms/lms/MainActivity.java
@@ -3,7 +3,6 @@ package uk.ac.gla.dcs.gms.lms;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -35,17 +34,17 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
             findViewById(R.id.back).setOnClickListener(this);
 
-            /*((ImageView)findViewById(R.id.imageView1)).setImageResource(R.drawable.bonus_coin);
+            ((ImageView)findViewById(R.id.imageView1)).setImageResource(R.drawable.bonus_coin);
             ((ImageView)findViewById(R.id.imageView2)).setImageResource(R.drawable.bonus_coin);
             ((ImageView)findViewById(R.id.imageView3)).setImageResource(R.drawable.bonus_coin);
             ((ImageView)findViewById(R.id.imageView4)).setImageResource(R.drawable.bonus_coin);
             ((ImageView)findViewById(R.id.imageView5)).setImageResource(R.drawable.bonus_coin);
-            ((ImageView)findViewById(R.id.imageView6)).setImageResource(R.drawable.bonus_coin);*/
+            ((ImageView)findViewById(R.id.imageView6)).setImageResource(R.drawable.bonus_coin);
         }
     }
 
     private void loginScreen() {
-        //((ImageView)findViewById(R.id.logo)).setImageResource(R.drawable.bonus_coin);
+        ((ImageView)findViewById(R.id.logo)).setImageResource(R.drawable.bonus_coin);
 
         TextView textView = (TextView)(findViewById(R.id.description));
         textView.setText("Description Description Description...");
