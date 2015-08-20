dd7a68ae411b15aac91795e7181a92bfce3d8aba
brunohms <bruno_h_m_s@hotmail.com>
2015-08-20 23:29:24 +0100

    bug fix

--- app/src/main/java/uk/ac/gla/dcs/gms/lms/LMSDailySummary.java
+++ app/src/main/java/uk/ac/gla/dcs/gms/lms/LMSDailySummary.java
@@ -1,8 +1,14 @@
 package uk.ac.gla.dcs.gms.lms;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -11,11 +17,15 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -60,7 +70,7 @@ public class LMSDailySummary extends GMSMainFragment implements AbsListView.OnSc
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        localListener = new LocalListener();
+
         imgList = new HashSet();
         imgViewList = new ArrayList<>();
         context = inflater.getContext();
@@ -68,6 +78,7 @@ public class LMSDailySummary extends GMSMainFragment implements AbsListView.OnSc
         View rootView;
 
         rootView = inflater.inflate(R.layout.lms_daily_summary, container, false);
+        localListener = new LocalListener(rootView);
 
         mHandler = new Handler();
 
@@ -94,7 +105,7 @@ public class LMSDailySummary extends GMSMainFragment implements AbsListView.OnSc
             //todo handle error...
         }
 
-        LMSImageRequestParamBuilder builder = new LMSImageRequestParamBuilder();
+        LMSImageRequestParamBuilder builder = new LMSImageRequestParamBuilder(context);
         builder.setLimit(10).setPersonal(true);
 
 
@@ -120,7 +131,7 @@ public class LMSDailySummary extends GMSMainFragment implements AbsListView.OnSc
 
     @Override
     public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-        LMSImageRequestParamBuilder builder = new LMSImageRequestParamBuilder();
+        LMSImageRequestParamBuilder builder = new LMSImageRequestParamBuilder(context);
 
         begin = Calendar.getInstance();
         end = Calendar.getInstance();
@@ -165,6 +176,17 @@ public class LMSDailySummary extends GMSMainFragment implements AbsListView.OnSc
     }
 
     private class LocalListener implements View.OnClickListener, HTTPResponseListener, Runnable {
+
+        private Animator mCurrentAnimator;
+        private View parent;
+        private Integer mShortAnimationDuration;
+
+        public LocalListener(View view) {
+            parent = view;
+
+            mShortAnimationDuration = 0;//getResources().getInteger()
+        }
+
         @Override
         public void onClick(View v) {
 
@@ -172,6 +194,10 @@ public class LMSDailySummary extends GMSMainFragment implements AbsListView.OnSc
             bundle.putString(ImageScrollerAdapter.ARG_IMG_URL, (String) v.getTag());
             Intent intent = new Intent(context, SingleViewActivity.class);
             intent.putExtras(bundle);
+
+            //edit here for zoom effect on image
+            //zoomImageFromThumb(v, );
+
             context.startActivity(intent);
         }
 
@@ -225,9 +251,146 @@ public class LMSDailySummary extends GMSMainFragment implements AbsListView.OnSc
 
         @Override
         public void run() {
-            LMSImageRequestParamBuilder builder = new LMSImageRequestParamBuilder();
+            LMSImageRequestParamBuilder builder = new LMSImageRequestParamBuilder(context);
             builder.setLimit(10).setPersonal(true).setSkip(imgList.size());
             lmsSession.getImages(localListener, 1, builder.toString());
+        }
+
+        private void zoomImageFromThumb(final View thumbView, String imageResId) {
+            // If there's an animation in progress, cancel it
+            // immediately and proceed with this one.
+            if (mCurrentAnimator != null) {
+                mCurrentAnimator.cancel();
+            }
+
+            // Load the high-resolution "zoomed-in" image.
+            final ImageView expandedImageView = (ImageView) parent.findViewById(R.id.lms_daily_summary_expanded_image);
+            Picasso.with(parent.getContext()).load(imageResId).into((ImageView) thumbView);
+
+            // Calculate the starting and ending bounds for the zoomed-in image.
+            // This step involves lots of math. Yay, math.
+            final Rect startBounds = new Rect();
+            final Rect finalBounds = new Rect();
+            final Point globalOffset = new Point();
+
+            // The start bounds are the global visible rectangle of the thumbnail,
+            // and the final bounds are the global visible rectangle of the container
+            // view. Also set the container view's offset as the origin for the
+            // bounds, since that's the origin for the positioning animation
+            // properties (X, Y).
+            thumbView.getGlobalVisibleRect(startBounds);
+            parent.findViewById(R.id.container)
+                    .getGlobalVisibleRect(finalBounds, globalOffset);
+            startBounds.offset(-globalOffset.x, -globalOffset.y);
+            finalBounds.offset(-globalOffset.x, -globalOffset.y);
+
+            // Adjust the start bounds to be the same aspect ratio as the final
+            // bounds using the "center crop" technique. This prevents undesirable
+            // stretching during the animation. Also calculate the start scaling
+            // factor (the end scaling factor is always 1.0).
+            float startScale;
+            if ((float) finalBounds.width() / finalBounds.height()
+                    > (float) startBounds.width() / startBounds.height()) {
+                // Extend start bounds horizontally
+                startScale = (float) startBounds.height() / finalBounds.height();
+                float startWidth = startScale * finalBounds.width();
+                float deltaWidth = (startWidth - startBounds.width()) / 2;
+                startBounds.left -= deltaWidth;
+                startBounds.right += deltaWidth;
+            } else {
+                // Extend start bounds vertically
+                startScale = (float) startBounds.width() / finalBounds.width();
+                float startHeight = startScale * finalBounds.height();
+                float deltaHeight = (startHeight - startBounds.height()) / 2;
+                startBounds.top -= deltaHeight;
+                startBounds.bottom += deltaHeight;
+            }
+
+            // Hide the thumbnail and show the zoomed-in view. When the animation
+            // begins, it will position the zoomed-in view in the place of the
+            // thumbnail.
+            thumbView.setAlpha(0f);
+            expandedImageView.setVisibility(View.VISIBLE);
+
+            // Set the pivot point for SCALE_X and SCALE_Y transformations
+            // to the top-left corner of the zoomed-in view (the default
+            // is the center of the view).
+            expandedImageView.setPivotX(0f);
+            expandedImageView.setPivotY(0f);
+
+            // Construct and run the parallel animation of the four translation and
+            // scale properties (X, Y, SCALE_X, and SCALE_Y).
+            AnimatorSet set = new AnimatorSet();
+            set
+                    .play(ObjectAnimator.ofFloat(expandedImageView, View.X,
+                            startBounds.left, finalBounds.left))
+                    .with(ObjectAnimator.ofFloat(expandedImageView, View.Y,
+                            startBounds.top, finalBounds.top))
+                    .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_X,
+                            startScale, 1f)).with(ObjectAnimator.ofFloat(expandedImageView,
+                    View.SCALE_Y, startScale, 1f));
+            set.setDuration(mShortAnimationDuration);
+            set.setInterpolator(new DecelerateInterpolator());
+            set.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mCurrentAnimator = null;
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+                    mCurrentAnimator = null;
+                }
+            });
+            set.start();
+            mCurrentAnimator = set;
+
+            // Upon clicking the zoomed-in image, it should zoom back down
+            // to the original bounds and show the thumbnail instead of
+            // the expanded image.
+            final float startScaleFinal = startScale;
+            expandedImageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (mCurrentAnimator != null) {
+                        mCurrentAnimator.cancel();
+                    }
+
+                    // Animate the four positioning/sizing properties in parallel,
+                    // back to their original values.
+                    AnimatorSet set = new AnimatorSet();
+                    set.play(ObjectAnimator
+                            .ofFloat(expandedImageView, View.X, startBounds.left))
+                            .with(ObjectAnimator
+                                    .ofFloat(expandedImageView,
+                                            View.Y,startBounds.top))
+                            .with(ObjectAnimator
+                                    .ofFloat(expandedImageView,
+                                            View.SCALE_X, startScaleFinal))
+                            .with(ObjectAnimator
+                                    .ofFloat(expandedImageView,
+                                            View.SCALE_Y, startScaleFinal));
+                    set.setDuration(mShortAnimationDuration);
+                    set.setInterpolator(new DecelerateInterpolator());
+                    set.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            thumbView.setAlpha(1f);
+                            expandedImageView.setVisibility(View.GONE);
+                            mCurrentAnimator = null;
+                        }
+
+                        @Override
+                        public void onAnimationCancel(Animator animation) {
+                            thumbView.setAlpha(1f);
+                            expandedImageView.setVisibility(View.GONE);
+                            mCurrentAnimator = null;
+                        }
+                    });
+                    set.start();
+                    mCurrentAnimator = set;
+                }
+            });
         }
     }
 }
